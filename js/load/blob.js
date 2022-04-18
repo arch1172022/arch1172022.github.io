137b9efcb6c3e8ccb8d97c0c681f22e547b1e10f
lihugang <lihugang@outlook.com>
2022-04-18 18:09:44 +0800

    Publish release:@1.0.2Add Function: Download

--- js/load/blob.js
+++ js/load/blob.js
@@ -1,10 +1,10 @@
 /**
  * @Author          : lihugang
  * @Date            : 2022-04-16 18:08:08
- * @LastEditTime    : 2022-04-17 15:17:09
+ * @LastEditTime    : 2022-04-18 18:08:57
  * @LastEditors     : lihugang
  * @Description     : 
- * @FilePath        : e:\arch117\vpn\js\load\blob.js
+ * @FilePath        : e:\arch117\vpn\arch1172022.github.io\js\load\blob.js
  * @Copyright (c) lihugang
  * @长风破浪会有时 直挂云帆济沧海
  * @There will be times when the wind and waves break, and the sails will be hung straight to the sea.
@@ -13,36 +13,36 @@
  * @Whether it's right or wrong, success or failure, it's all empty now, and it's all gone with the passage of time. The green hills of the year still exist, and the sun still rises and sets.
  */
 rate_limit();
-if (window.db.password){
+if (window.db.password) {
 	if (window.db.username)
-		EL(function(){
+		EL(function () {
 			document.querySelector("#userinfo").innerHTML = "<b><a onclick=javascript:window.open('/settings/','_blank');><img id=avatar height=64px width=64px src=" + localStorage.avatar_url + "><br>" + window.db.username + "</a></b><br>------<br><button onclick=javascript:logout();>Logout</button><br><a href=# onclick=javascript:back();>Back</a>";
-			
+
 			if (window.db.repos) {
 				var args = ParseURLArgs();
 				console.log(args);
-				var repo_id = args.repo_id; 
+				var repo_id = args.repo_id;
 				var repo_data = window.db["repos_" + repo_id];
-				if (typeof(repo_data) == "undefined") {
+				if (typeof (repo_data) == "undefined") {
 					document.querySelector("#workspace").innerHTML = "<font color=red>The project is not in the local database.</font><br><a href=# onclick=javascript:getrepodata(" + repo_id + ");>Fetch</a>";
 					return -1;
 				} else repo_data = JSON.parse(repo_data);
-				document.querySelector("#workspace").innerHTML = "<b>" + repo_data.name + "</b>/<a href=# onclick=javascript:TomasterBranch();>main</a>/<span id=path>" + args.path + "</span><p id=tools style='text-align:right'><input type=button value='Edit' onclick=javascript:go_edit_file(); id=edit_button><input type=button value='Open it on GitHub' onclick=javascript:open_github(); id=edit_button><input type=button value='Download' id=download_button disabled><input type=button value='Delete' onclick=javascript:go_delete_file();></p><hr><div id=content>Fetching your file from <font color=red>api.github.com</font>.<br>Please wait a moment.</div>";
+				document.querySelector("#workspace").innerHTML = "<b>" + repo_data.name + "</b>/<a href=# onclick=javascript:TomasterBranch();>main</a>/<span id=path>" + args.path + "</span><p id=tools style='text-align:right'><input type=button value='Edit' onclick=javascript:go_edit_file(); id=edit_button><input type=button value='Open it on GitHub' onclick=javascript:open_github(); id=edit_button><input type=button value='Download' id=download_button disabled onclick=javascript:download_file();><input type=button value='Delete' onclick=javascript:go_delete_file();></p><hr><div id=content>Fetching your file from <font color=red>api.github.com</font>.<br>Please wait a moment.</div>";
 				buildPathLink();
 				var req_url = args.url;
 				req_url = req_url.substring(23).split("?")[0];
 				window.current_file = req_url;
-				getfile(req_url).then(function(res){
+				getfile(req_url).then(function (res) {
 					var info = JSON.parse(res.response);
-					console.log(res,info);
-					if (typeof(info.content) == "undefined"){
+					console.log(res, info);
+					if (typeof (info.content) == "undefined") {
 						var dat = location.href.substring(location.origin.length + 6);
 						location.href = "/tree/" + dat;
 					};
 					if (info.encoding == "base64") {
 						var content = utf8to16(atob(info.content));
 						console.log(content);
-						var link = req_url.split("?")[0].split(".");	
+						var link = req_url.split("?")[0].split(".");
 						var suffix = link[link.length - 1];
 
 						if (suffix == "jpg" || suffix == "png" || suffix == "webp" || suffix == "bmp" || suffix == "jpeg" || suffix == "ico" || suffix == "gif" || suffix == "svg") {
@@ -58,7 +58,7 @@ if (window.db.password){
 							var dataurl = "data:audio/" + suffix + ";base64," + info.content;
 							document.querySelector("#content").innerHTML = "<audio src=" + dataurl + " controls>";
 						} else {
-							if (isBinaryFile(content)){
+							if (isBinaryFile(content)) {
 								document.querySelector("#edit_button").disabled = true;
 								document.querySelector("#content").innerHTML = "<p style='text-align:center'>Binary File</p>";
 								window.current_file = req_url;
@@ -66,9 +66,11 @@ if (window.db.password){
 								document.querySelector("#content").innerHTML = "<textarea id=code readonly cols=200 rows=120></textarea>";
 								document.querySelector("#code").value = content;
 							};
-						};			
+						};
+						window.download_data = "data:application/octet-stream;base64," + info.content;
+						document.querySelector("#download_button").disabled = false;
 					};
-				}).catch(function(err){
+				}).catch(function (err) {
 					console.error(err);
 					if (err.status == 401 || err.status == 404) {
 						document.querySelector("#workspace").innerHTML = "<font color=red>Failed to load data.</font><br><a href=# onclick=javascript:location.reload();>Reload</a>";
@@ -78,7 +80,7 @@ if (window.db.password){
 					} else if (err.status == 403) {
 						//File is too large
 						document.querySelector("#edit_button").disabled = true;
-						if (window.XMLHttpRequest){
+						if (window.XMLHttpRequest) {
 							var xhr = new XMLHttpRequest();
 						} else {
 							var xhr = new ActiveXObject("Microsoft.XMLHTTP");
@@ -87,22 +89,22 @@ if (window.db.password){
 						var url = args.url.split("?")[0].split("/");
 						var i = 4;
 						var link = "https://raw.githubusercontent.com/";
-						while (i < url.length){
-							if (i == 4 || i > 6)	
+						while (i < url.length) {
+							if (i == 4 || i > 6)
 								link += url[i] + "/";
 							if (i == 5)
 								link += url[i] + "/master/";
 							i++;
 						};
-						link = link.substring(link.length-1,-1);
-						var suffix = url[url.length-1].split(".");
-						suffix = suffix[suffix.length - 1];							
+						link = link.substring(link.length - 1, -1);
+						var suffix = url[url.length - 1].split(".");
+						suffix = suffix[suffix.length - 1];
 						document.querySelector("#content").innerHTML = "<font color=red>File is too large. (More than 1MB)</font><br>Fetching your file from <font color=red>raw.githubusercontent.com</font>......<br>Please wait a moment.";
 						console.log(link);
-						xhr.open("GET",link,true);
+						xhr.open("GET", link, true);
 						xhr.send();
-						xhr.onload = function(){
-							if (xhr.status >= 200 && xhr.status < 400){
+						xhr.onload = function () {
+							if (xhr.status >= 200 && xhr.status < 400) {
 								var content = xhr.response.toutf().toBuffer().decode().toString();
 								if (suffix == "jpg" || suffix == "png" || suffix == "webp" || suffix == "bmp" || suffix == "jpeg" || suffix == "ico" || suffix == "gif" || suffix == "svg") {
 									document.querySelector("#edit_button").disabled = true;
@@ -117,7 +119,7 @@ if (window.db.password){
 									var dataurl = "data:audio/" + suffix + ";base64," + info.content;
 									document.querySelector("#content").innerHTML = "<audio src=" + dataurl + " controls>";
 								} else {
-									if (isBinaryFile(content)){
+									if (isBinaryFile(content)) {
 										document.querySelector("#edit_button").disabled = true;
 										document.querySelector("#content").innerHTML = "<p style='text-align:center'>Binary File</p>";
 										window.current_file = req_url;
@@ -125,8 +127,8 @@ if (window.db.password){
 										document.querySelector("#content").innerHTML = "<textarea id=code readonly cols=200 rows=120></textarea>";
 										document.querySelector("#code").value = content;
 									};
-								};			
-		
+								};
+
 							} else {
 								document.querySelector("#workspace").innerHTML = "<font color=red>Failed to load data.</font><br><a href=# onclick=javascript:location.reload();>Reload</a>";
 								document.querySelector("#delete_button").disabled = true;
@@ -140,137 +142,125 @@ if (window.db.password){
 				location.reload();
 			};
 		});
-	 else EL(function(){
-			document.querySelector("#userinfo").innerHTML = "<button onclick=javascript:logout();>Logout</button>";
-			getfile("user").then(function(res){
-				var dat = JSON.parse(res.response);
-				console.log(res,dat);
-				localStorage.username = dat.login;
-				localStorage.userid = dat.id;
-				localStorage.avatar_url = dat.avatar_url;
-				location.reload();
-			}).catch(function(err){
-				console.error(err);
-				alert("Failed to load your user data.");
-			});
+	else EL(function () {
+		document.querySelector("#userinfo").innerHTML = "<button onclick=javascript:logout();>Logout</button>";
+		getfile("user").then(function (res) {
+			var dat = JSON.parse(res.response);
+			console.log(res, dat);
+			localStorage.username = dat.login;
+			localStorage.userid = dat.id;
+			localStorage.avatar_url = dat.avatar_url;
+			location.reload();
+		}).catch(function (err) {
+			console.error(err);
+			alert("Failed to load your user data.");
 		});
+	});
 	;
 } else {
-	EL(function(){
+	EL(function () {
 		document.querySelector("#userinfo").innerHTML = "Please login first.";
 		document.querySelector("#workspace").innerHTML = "<center><p style='font-size:1.5em'>117 Archives Verification System<br>Please input your Key<br><input type=text id=github_access_token placeholder=xxxxxxx style='width:400px'></p><br><button onclick=login();>Confirm</button></center>";
 	});
 };
-function back(){
+function back() {
 	var args = buildBackURL(ParseURLArgs());
-	if (typeof(args) == "string")
+	if (typeof (args) == "string")
 		location.href = args;
 	else
 		location.href = "/tree/?repo_id=" + args.repo_id + "&path=" + args.path + "&url=" + args.url;
 };
-function buildBackURL(args){
+function buildBackURL(args) {
 	if (args.path.indexOf("/") == -1)
 		return "/repo/?id=" + args.repo_id;
 	else {
 		var last_dict_position = args.path.lastIndexOf("/");
-		var current_path = args.path.substring(0,last_dict_position);
+		var current_path = args.path.substring(0, last_dict_position);
 		var cau = args.url.split("?")[0];
 		var cau_ldp = cau.lastIndexOf("/");
-		cau = cau.substring(0,cau_ldp);
-		return {repo_id:args.repo_id,path:current_path,url:cau};
-	};	
+		cau = cau.substring(0, cau_ldp);
+		return { repo_id: args.repo_id, path: current_path, url: cau };
+	};
 };
-function TomasterBranch(){
+function TomasterBranch() {
 	var args = ParseURLArgs();
 	location.href = "/repo/?id=" + args.repo_id;
 };
-function buildPathLink(){
+function buildPathLink() {
 	var path = document.querySelector("#path").innerHTML;
 	path = path.split("/");
 	var current_path = "";
 	var str = "";
 	var i = 0;
-	while (i < path.length){
+	while (i < path.length) {
 		current_path += path[i] + "/";
-		str += "<a class=path_link data-link=" + current_path.substring(current_path.length-1,-1) + ">" + path[i] + "</a>/";
+		str += "<a class=path_link data-link=" + current_path.substring(current_path.length - 1, -1) + ">" + path[i] + "</a>/";
 		i++;
 	};
-	document.querySelector("#path").innerHTML = str.substring(str.length-1,-1);
+	document.querySelector("#path").innerHTML = str.substring(str.length - 1, -1);
 	var eles = document.querySelectorAll(".path_link");
 	i = eles.length - 1;
 	var args = ParseURLArgs();
-	while (i >= 0){
+	while (i >= 0) {
 		eles[i].href = "/tree/?repo_id=" + args.repo_id + "&path=" + args.path + "&url=" + args.url;
 		args = buildBackURL(args);
 		i--;
 	};
-	eles[eles.length-1].href = "/blob/" + eles[eles.length-1].href.substring(6 + location.origin.length);
-	
+	eles[eles.length - 1].href = "/blob/" + eles[eles.length - 1].href.substring(6 + location.origin.length);
+
 };
-function download_file(){
-	var blob_url = geturl();
-	var suffix = blob_url.split("/");
-	suffix = suffix[suffix.length-1].split(".");
-	suffix = suffix[suffix.length - 1];
-	if (suffix == "jpg" || suffix == "png" || suffix == "webp" || suffix == "bmp" || suffix == "jpeg" || suffix == "ico" || suffix == "gif" || suffix == "svg" || suffix == "mp3" || suffix == "wmv" || suffix == "mp4" || suffix == "pdf") {
-		blob_url = replaceURL(blob_url);
-		window.open(blob_url,"_blank");
-	} else {
-		var ele = document.createElement("a");
-		ele.href = blob_url;
-		var args = ParseURLArgs();
-		var url = args.url.split("?")[0];
-		url = url.split("/");
-		ele.download = url[url.length - 1]
-		ele.click();
-	};
+function download_file() {
+	var ele = document.createElement("a");
+	ele.href = window.download_data;
+	ele.download = document.querySelectorAll(".path_link")[document.querySelectorAll(".path_link").length-1].innerHTML;
+	ele.click();
 };
-function geturl(){
-	if (typeof(window.current_file) == "string") {
+function geturl() {
+	if (typeof (window.current_file) == "string") {
 		var url = "https://api.github.com/" + window.current_file;
 		url = url.split("?")[0].split("/");
 		var i = 4;
 		var link = "https://raw.githubusercontent.com/";
-		while (i < url.length){
-			if (i == 4 || i > 6)	
+		while (i < url.length) {
+			if (i == 4 || i > 6)
 				link += url[i] + "/";
 			if (i == 5)
 				link += url[i] + "/master/";
 			i++;
 		};
-		link = link.substring(link.length-1,-1);
-		console.log(url,link,window.current_file);
+		link = link.substring(link.length - 1, -1);
+		console.log(url, link, window.current_file);
 		return link;
 	} else return window.URL.createObjectURL(window.current_file);
-	
+
 };
-function isBinaryFile(content){
+function isBinaryFile(content) {
 	var len = content.length;
 	var bin_char = 0;
 	var i = 0;
-	while (i < content.length){
+	while (i < content.length) {
 		var ascii_code = content.charCodeAt(i);
 		if ((ascii_code < 32) && (content[i] != "\n" && content[i] != "\r" && content[i] != "\t" && content[i] != "\b"))
 			bin_char++;
 		i++;
 	};
 	var ratio = bin_char / len;
-	console.log(bin_char,len,ratio);
+	console.log(bin_char, len, ratio);
 	return (ratio > 0.15);
 };
-function go_edit_file(){
+function go_edit_file() {
 	var dat = location.href.substring(location.origin.length + 6);
 	location.href = "/edit/" + dat;
 };
-function go_delete_file(){
+function go_delete_file() {
 	var dat = location.href.substring(location.origin.length + 6);
 	location.href = "/delete/" + dat;
 };
-function open_github(){
+function open_github() {
 	var path = "https://www.github.com/";
 	var args = ParseURLArgs();
 	path += JSON.parse(window.db["repos_" + args.repo_id]).fullname;
 	path += "/blob/master/";
 	path += args.path;
-	window.open(path,"_blank");
+	window.open(path, "_blank");
 };
